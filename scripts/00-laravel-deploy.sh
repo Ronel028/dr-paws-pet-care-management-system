533cfbc03e03d231b54479627d0a0ec92da65d2d
Ronel028 <floridaronel15@gmail.com>
2024-08-18 06:35:05 +0800

    add server setup

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+# echo "Installing Node.js dependencies"
+# npm install --no-dev --working-dir=/var/www/html
+
+echo "Building assets"
+npm run build --include=dev --working-dir=/var/www/html
+
+echo "generating application key..."
+php artisan key:generate --show
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "link pubic storage"
+php artisan storage:link
